74e5888fe38a9c8ded7e645c3f66bd4b77c1c338
lobsteropteryx <ian.firkin@gmail.com>
2015-11-19 22:08:27 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/api/v1/beaches', function (req, res) {
+  res.send(JSON.stringify([{name: 'Beach1', nests: '/api/v1/nests/?beach=Beach1'}]));
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
